84b934a0531b121ef8d954e960977ee40d9b4c07
vmeli <dev@gmail.com>
2019-06-24 23:46:53 -0500

    adding-stateless-components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,39 @@ import React from 'react';
 import Curso from './Curso';
 import './styles/app.scss';
 
+const lista =[
+  {
+    title: 'Cachorro',
+    image: 'https://picsum.photos/id/237/200/300',
+    categoria: 'mascotas'
+  },
+  {
+    title: 'Recuerdos',
+    image: 'https://picsum.photos/id/320/200/300',
+    categoria: 'atardecer'
+  }, 
+  {
+    title: 'Infancia',
+    image: 'https://picsum.photos/id/820/200/300',
+    categoria: 'columpio'
+  }
+]
+
 const App = () => (
   <>
     <h1>Ejemplos de card</h1>
-    <Curso 
-      //title="Cachorro"
-      //image="https://picsum.photos/id/237/200/300"
-      //categoria="mascotas"
-    />
+    {
+      lista.map( item => <Curso title={item.title}
+                                image={item.image}
+                                categoria={item.categoria}
+                          />
+      )
+    }
+    {/* <Curso 
+      title="Cachorro"
+      image="https://picsum.photos/id/237/200/300"
+      categoria="mascotas"
+    /> */}
   </>
 )
 
